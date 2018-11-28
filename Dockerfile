f01147a123c0e94e8077b795662031175ba04ee5
mohamed-aziz <medazizknani@gmail.com>
2018-11-28 19:41:53 +0100

    Update production config

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,24 @@ FROM python:3.6
 LABEL maintainer="medazizknani@gmail.com"
 
 ENV PYTHONUNBUFFERED 1
-ENV DJANGO_ENV dev
+ENV DJANGO_ENV production
+ENV DJANGO_SETTINGS_MODULE cybertrace.settings.production
 
 COPY ./requirements.txt /code/requirements.txt
+
+RUN apt-get update && apt-get install python3-mysqldb mysql-client dnsutils -y 
 RUN pip install -r /code/requirements.txt
 RUN pip install gunicorn
 
 COPY . /code/
 WORKDIR /code/
 
-RUN python manage.py migrate
+# RUN python manage.py makemigrations
+# RUN python manage.py migrate
 
-RUN useradd wagtail
-RUN chown -R wagtail /code
-USER wagtail
+# RUN useradd wagtail
+# RUN chown -R wagtail /code
+# USER wagtail
 
 EXPOSE 8000
-CMD exec gunicorn cybertrace.wsgi:application --bind 0.0.0.0:8000 --workers 3
+CMD exec gunicorn cybertrace.wsgi:application --bind 0.0.0.0:8000 --workers 3 --access-logfile /var/log/web.access.log --error-logfile /var/log/web.error.log
--- cybertrace/settings/production.py
+++ cybertrace/settings/production.py
@@ -8,11 +8,51 @@ DATABASES = {
         'NAME': 'cybertrace_web',
         'USER':'cybertrace',
         'PASSWORD':'cybertrace',
-        'HOST':'cybertrace_db',
+        'HOST':'db',
+        'PORT': 3306,
     }
 }
 
-#ALLOWED_HOSTS = ['nginx']
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'applogfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'cybertrace.log'),
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10,
+        },
+        
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['applogfile'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'cybertrace': {
+            'handlers': ['applogfile',],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+with open('.secret_key', 'rb') as myfile:    
+    SECRET_KEY = myfile.read()
+
+ALLOWED_HOSTS = ['*']
 
 try:
     from .local import *
